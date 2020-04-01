872654a966fa8184279732057f13cbde21680f6e
Mack Maier <hmaiermack@gmail.com>
2020-03-31 19:10:41 -0700

    update deploy

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DB_URL || 'postgresql://postgres:candid55@localhost/noteful',
-    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres:candid55@localhost/blogful-test'
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres:candid55@localhost/noteful-test'
   }
